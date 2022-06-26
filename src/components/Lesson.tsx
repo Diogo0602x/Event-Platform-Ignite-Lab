282b23634e99260e28935f2f417ae6514115a2e8
Diogo Sales Teixeira <diogoteixeira0602@gmail.com>
2022-06-26 15:34:30 -0300

    Aula 3 terminada

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,6 +1,7 @@
 import { CheckCircle, Lock } from 'phosphor-react'
 import { isPast, format } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
+import { Link } from 'react-router-dom';
 
 interface LessonProps {
   title: string;
@@ -16,12 +17,12 @@ export function Lesson(props: LessonProps) {
   });
 
   return (
-    <a href="#">
+    <Link to={`/event/lesson/${props.slug}`} className="group">
       <span className="text-gray-300">
         {availableDateFormatted}
       </span>
 
-      <div className="rounder border border-gray p-4 mt-2">
+      <div className="rounder border border-gray p-4 mt-2 group-hover:border-green-500">
         <header className="flex items-center justify-between">
           {isLessonAvailable ? (
             <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
@@ -43,6 +44,6 @@ export function Lesson(props: LessonProps) {
           {props.title}
         </strong>
       </div>
-    </a>
+    </Link>
   )
 }
